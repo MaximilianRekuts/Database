9cf3fa972da819bf4873aae087ea5e77ad7eb70f
Maximilian Rekuts <51362660+MaximilianRekuts@users.noreply.github.com>
2022-02-02 11:41:09 +0100

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -3,34 +3,34 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
-    def drop_tables(cur, conn):
-"""
-this function iterates over the drop table queries and executes
-inputs:
-- cur the cursor variable of the database
-- conn the connection variable of the database
-"""
+def drop_tables(cur, conn):
+    """
+    this function iterates over the drop table queries and executes
+    inputs:
+    - cur the cursor variable of the database
+    - conn the connection variable of the database
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-    def create_tables(cur, conn):
-"""
-this function iterates over the create table queries and executes
-inputs:
-- cur the cursor variable to the database
-- conn the connection variable to the database
-"""
+def create_tables(cur, conn):
+    """
+    this function iterates over the create table queries and executes
+    inputs:
+    - cur the cursor variable to the database
+    - conn the connection variable to the database
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-    def main():
-"""
-this function connects to the database using the credentials in the config file and then drops and recreates the required tables.
-"""
+def main():
+    """
+    this function connects to the database using the credentials in the config file and then drops and recreates the required tables.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
