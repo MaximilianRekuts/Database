100100dc5c38b2a2ea7de7fe10f46f2e57c3c897
Maximilian Rekuts <51362660+MaximilianRekuts@users.noreply.github.com>
2022-02-02 11:39:57 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,34 +4,34 @@ import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 
-    def load_staging_tables(cur, conn):
-"""
-iterates over a list of load queries that load the data from the s3 bucket to the stage tables using COPY command
-inputs:
-- cur the cursor variable of the database
-- conn the connection variable of the database
-"""
+def load_staging_tables(cur, conn):
+    """
+    iterates over a list of load queries that load the data from the s3 bucket to the stage tables using COPY command
+    inputs:
+    - cur the cursor variable of the database
+    - conn the connection variable of the database
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-    def insert_tables(cur, conn):
-"""
-iterates over a list of insert queries that insert the data from stage table to final table
-inputs:
-- cur the cursor variable of the database
-- conn the connection variable of the database
-"""
+def insert_tables(cur, conn):
+    """
+    iterates over a list of insert queries that insert the data from stage table to final table
+    inputs:
+    - cur the cursor variable of the database
+    - conn the connection variable of the database
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-    def main():
-"""
-reads the db credentials from the config file, connects to the database, loads the s3 files into stage tables, loads the final tables from stage tables and then disconnects from the database
-"""
+def main():
+    """
+    reads the db credentials from the config file, connects to the database, loads the s3 files into stage tables, loads the final tables from stage tables and then disconnects from the database
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
